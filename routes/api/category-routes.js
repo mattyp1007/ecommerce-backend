2355c67987ad05b0f9566fea9c0c65d807e174ae
Matthew Parker <matt.parker1007@gmail.com>
2021-04-29 20:52:12 -0500

    completed category PUT and DELETE routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,12 +50,46 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const catData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!catData[0]) {
+      res.status(404).json({ message: "Error: Category not found!"});
+      return;
+    }
+
+    res.status(200).json(catData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if(!catData){
+      res.status(404).json({ message: "Error: Category not found!" });
+      return;
+    }
+
+    res.status(200).json(catData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
